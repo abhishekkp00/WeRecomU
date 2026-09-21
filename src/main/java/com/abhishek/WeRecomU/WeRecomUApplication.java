b1daec4dfa03dec9da675a00f0df3de922b40de6
abhishekkp00 <abhishekforcollege@gmail.com>
2026-09-21 21:27:31 +0530

    Start: Started with the Recommendation Project

---
+++ src/main/java/com/abhishek/WeRecomU/WeRecomUApplication.java
@@ -0,0 +1,13 @@
+package com.abhishek.WeRecomU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeRecomUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeRecomUApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhishek/WeRecomU/WeRecomUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishek.WeRecomU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeRecomUApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
